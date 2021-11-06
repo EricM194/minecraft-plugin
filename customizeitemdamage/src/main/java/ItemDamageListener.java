7caf5d0f26c6b860059bcd734c36261dae054143
AR <AR@home>
2021-11-06 11:59:07 -0300

    Initial Commit For Customizing Item Damage Chance

---
+++ customizeitemdamage/src/main/java/ItemDamageListener.java
@@ -0,0 +1,20 @@
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+
+public class ItemDamageListener implements Listener {
+
+    @EventHandler
+    public void onPlayerItemDamageEvent(PlayerItemDamageEvent event) {
+        try {
+            Integer damageChancePercentage = Hook.itemDamageChanceMap.get(event.getItem().getType().toString());
+            if (damageChancePercentage != null &&
+                    Math.random()*100 > damageChancePercentage
+            ) {
+                event.setCancelled(true);
+            }
+        } catch (Exception ignored) {
+        }
+
+    }
+}
