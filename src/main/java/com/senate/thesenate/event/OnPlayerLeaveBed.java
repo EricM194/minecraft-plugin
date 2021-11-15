1d07e87f4c347d0b0a2a0a7756bde4f77f299880
Eric M <ericmichelin194@gmail.com>
2021-11-14 23:27:44 -0400

    added missing EventHandler

--- src/main/java/com/senate/thesenate/event/OnPlayerLeaveBed.java
+++ src/main/java/com/senate/thesenate/event/OnPlayerLeaveBed.java
@@ -1,10 +1,13 @@
 package com.senate.thesenate.event;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 public class OnPlayerLeaveBed implements Listener {
+
+    @EventHandler
     public void onLeaveBed(PlayerBedLeaveEvent e) {
         Player player = e.getPlayer();
         player.sendMessage("Hey, you, you’re finally awake.");
